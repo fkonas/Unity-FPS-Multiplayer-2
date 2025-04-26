42b05b063b5e06380aa6d492a55d978ae7fe1a15
Ferhat Konas <fkonas@gmail.com>
2025-04-26 18:53:58 +0300

    Part 1 - Part2
    Bölüm 1: Introduction /////////////////////////////////
    1. Introduction
    
    Bölüm 2: The Player ///////////////////////////////////
    2. Setting Up
    3. Understanding Looking Around
    4. Making The Camera Move
    5. Looking Up & Down
    6. Inverting & Hiding The Mouse
    7. Making The Player Move
    8. Interacting With The World
    9. Moving At Different Speeds
    10. Setting Camera Position
    11. Adding Gravity
    12. Jumping
    13. Staying Grounded
    14. Building The Game To Test
    15. Freeing The Mouse

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Unity-FPS-Multiplayer-2
+- Unity version: Unity 6000.0.25f1
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSensitivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
+    public bool invertLook;
+
+    public float moveSpeed = 5f, runSpeed = 8f;
+    private float activeMoveSpeed;
+    private Vector3 moveDir, movement;
+
+    public CharacterController charCont;
+
+    private Camera cam;
+    public float jumpForce = 12f, gravityMod = 2.5f;
+
+    public Transform groundCheckPoint;
+    private bool isGrounded;
+    public LayerMask groundLayers;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60f, 60f);
+
+        if (invertLook)
+        {
+            viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+        else
+        {
+            viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            activeMoveSpeed = runSpeed;
+        }
+        else
+        {
+            activeMoveSpeed = moveSpeed;
+        }
+
+        float yVel = movement.y;
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+
+        if (!charCont.isGrounded)
+        {
+            movement.y = yVel;
+        }
+
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundLayers);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+
+        charCont.Move (movement * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Cursor.lockState == CursorLockMode.None)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
+    }
+}
